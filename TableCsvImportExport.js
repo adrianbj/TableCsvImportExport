d5dec179dcb127cf7bfd686a1daecae55af1fb2e
adrianbj <adrianbjones@gmail.com>
2014-10-06 20:16:38 -0700

    Added basic support for delimiter and names in first row for import and export.

--- TableCsvImportExport.js
+++ TableCsvImportExport.js
@@ -5,7 +5,14 @@ $(document).ready(function() {
     $(document).on('click', '.export_csv', function(){
         //$.get("/export_csv.php?pid="+$(this).attr('data-pageid')+"&fn="+$(this).attr('data-fieldname'));
         //window.open("/export_csv.php?pid="+$(this).attr('data-pageid')+"&fn="+$(this).attr('data-fieldname'), 'csv export');
-        $('#download').attr('src', "/"+$(this).attr('data-adminurl')+"/page/table-csv-export/?pid="+$(this).attr('data-pageid')+"&fn="+$(this).attr('data-fieldname'));
+        $('#download').attr('src', "/"+
+            $(this).attr('data-adminurl')+
+            "/page/table-csv-export/?pid="+
+            $(this).attr('data-pageid')+
+            "&fn="+$(this).attr('data-fieldname')+
+            "&cs="+$("#Inputfield_export_column_separator").val()+
+            "&nfr="+$("#Inputfield_export_names_first_row").attr('checked')
+        );
         return false;
     });
 });
