b83df4d13fd18fcb6c4bc8ac467e6ae35cba9711
adrianbj <adrianbjones@gmail.com>
2016-09-23 15:08:55 -0700

    Fix support for custom export settings when there are multiple Table fields on the one page.

--- TableCsvImportExport.js
+++ TableCsvImportExport.js
@@ -7,11 +7,11 @@ $(document).ready(function() {
             "setup/table-csv-export/?pid="+
             $(this).attr('data-pageid')+
             "&fn="+$(this).attr('data-fieldname')+
-            "&cs="+$("#Inputfield_export_column_separator").val()+
-            "&ce="+$("#Inputfield_export_column_enclosure").val()+
-            "&ext="+$("#Inputfield_export_extension").val()+
-            "&nfr="+$("#Inputfield_export_names_first_row").attr('checked')+
-            "&mvs="+$("#Inputfield_export_multiple_values_separator").val()
+            "&cs="+$("#Inputfield_"+$(this).attr('data-fieldname')+"_export_column_separator").val()+
+            "&ce="+$("#Inputfield_"+$(this).attr('data-fieldname')+"_export_column_enclosure").val()+
+            "&ext="+$("#Inputfield_"+$(this).attr('data-fieldname')+"_export_extension").val()+
+            "&nfr="+$("#Inputfield_"+$(this).attr('data-fieldname')+"_export_names_first_row").attr('checked')+
+            "&mvs="+$("#Inputfield_"+$(this).attr('data-fieldname')+"_export_multiple_values_separator").val()
         );
         return false;
     });
